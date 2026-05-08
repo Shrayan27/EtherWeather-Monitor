75f1e27cc69e4d8450889ab126d336e12f4683ed
Shrayan27 <shrayanmahajan@gmail.com>
2026-05-08 12:21:44 +0530

    commit

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import sys
+import os
 from PyQt6.QtWidgets import QApplication
-from Modbus_Gui import SetUpPage
+from PyQt6.QtGui import QIcon
+from Modbus_Gui import SetUpPage, resource_path
+
+if os.name == 'nt':
+    import ctypes
+    myappid = 'kbsensormart.weatherstation.gui.1.0'
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon(resource_path("favicon.png")))
     window = SetUpPage()
     window.show()
     sys.exit(app.exec())
